0664255b7f83ef7bf4452705d9d9d62957cc9c35
pd-9999 <51860817+pd-9999@users.noreply.github.com>
2019-11-06 13:11:35 +0800

    Have a working noughts and crosses game with a VERY dumb comp player

---
+++ XOBoard.h
@@ -0,0 +1,31 @@
+#ifndef GUARD_XOBoard_h
+#define GUARD_XOBoard_h
+
+//Header for for XOBoard class
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+struct XOBoard {
+    public:
+    void printBoard();
+    void playGame();
+
+    private:
+    std::vector<std::string> board;
+    std::vector< std::vector<int>> win_checks = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    int won = 0;
+
+    void initialiseBoard();
+    bool invalid(int);
+    void wonOrNot();
+};
+
+
+
+
+
+
+
+#endif
--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,61 +1,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "XOBoard.h"
 
 
 using std::cout; using std::cin;
 using std::endl; using std::string;
 using std::vector;
 
-
-struct XOBoard {
-    public:
-    void populateBoard();
-    void printBoard();
-
-    private:
-    std::vector<std::string> board;
-
-};
-
-void XOBoard::populateBoard(){
-
-    cout << "wit do you want in here?";
-
-    string temp;
-
-    for(int i = 0; i != 9; i++){
-
-        cin >> temp;
-        board.push_back(temp);
-        cout << "anuther" << endl;
-
-    }
-    
-}
-
-
-void XOBoard::printBoard(){
-
-   /*
-   The board will be displayed as shown below 
-    x | O | O
-    O | x | O
-    O | O | x   
-   */
-
-    for(int i = 0; i != board.size(); i++){
-
-        if(i%3 == 2){
-            cout << board[i] << endl;
-        }else{
-            cout <<  " " << "|";
-        }
-
-    }
-    
-
-}
 
 
 
@@ -64,8 +16,7 @@ int main()
 
     XOBoard game1;
 
-    game1.populateBoard();
-
+    game1.playGame();
     game1.printBoard();
 
 
